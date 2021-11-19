a837b4e0ae8769ca1f97ee6ee49f4b25e7efe62b
piyachandak1210 <priyankachandak@nimapinfotech.com>
2021-11-19 11:22:25 +0530

    updates

--- Componenets/Dashboard.js
+++ Componenets/Dashboard.js
@@ -1,15 +1,96 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
+import SQLite from 'react-native-sqlite-storage';
 
-import {View,Text,StyleSheet} from 'react-native';
 
-const Dashboard =({navigation})=>{
-return(
-    <View>
-        <Text>Hello</Text>
-    </View>
-)
+const db = SQLite.openDatabase(
+    {
+        name: 'MapDb',
+        location: 'default',
+    },
+    () => { },
+    error => { console.log(error) }
+);
+
+const Dashboard = ({ navigation }) => {
+    const [items, setItems] = useState([]);
+    const [empty, setEmpty] = useState([]);
+
+    useEffect(() => {
+        db.transaction((tx) => {
+            tx.executeSql(
+                'SELECT * FROM Location',
+                [],
+                (tx, results) => {
+                    var temp = [];
+                    for (let i = 0; i < results.rows.length; ++i)
+                        temp.push(results.rows.item(i));
+                    setItems(temp);
+
+                    if (results.rows.length >= 1) {
+                        setEmpty(false);
+                    } else {
+                        setEmpty(true)
+                    }
+
+                }
+            );
+
+        });
+    }, []);
+    const listViewItemSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 1,
+                    width: '100%',
+                    backgroundColor: '#000'
+                }}
+            />
+        );
+    };
+
+    const emptyMSG = (status) => {
+        return (
+            <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+
+                <Text style={{ fontSize: 20, textAlign: 'center' }}>
+                    No Record Inserted Database is Empty...
+                </Text>
+
+            </View>
+        );
+    }
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+                <Text style={{ fontSize: 20, textAlign: 'center', color: 'black', padding: 10 }}>
+                    My Location
+                </Text>
+                {empty ? emptyMSG(empty) :
+
+                    <FlatList
+                        data={items}
+                        ItemSeparatorComponent={listViewItemSeparator}
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={({ item }) =>
+                            <View key={item.student_id} style={{ padding: 20 }}>
+
+                                <Text style={styles.itemsStyle}> latitude: {item.latitude} </Text>
+                                <Text style={styles.itemsStyle}> longitude: {item.longitude} </Text>
+
+
+                            </View>
+                        }
+                    />
+                }
+            </View>
+        </SafeAreaView>
+
+    )
 }
 
-const styles=StyleSheet.create({});
+const styles = StyleSheet.create({});
 
 export default Dashboard;
--- Componenets/MapScreen.js
+++ Componenets/MapScreen.js
@@ -1,63 +1,120 @@
-import React,{useState,useEffect} from "react";
-import { StyleSheet, Text, View , PermissionsAndroid,} from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, PermissionsAndroid, } from "react-native";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
 import Geolocation from '@react-native-community/geolocation';
-import CustomButton from './CustomButton'
+import CustomButton from './CustomButton';
+import SQLite from 'react-native-sqlite-storage';
 
-export default function MapScreen({navigation}) {
+const db = SQLite.openDatabase(
+    {
+        name: 'MapDb',
+        location: 'default',
+    },
+    () => { },
+    error => { console.log(error) }
+);
+
+
+export default function MapScreen({ navigation }) {
     const [position, setPosition] = useState({
         latitude: 10,
         longitude: 10,
         latitudeDelta: 0.001,
         longitudeDelta: 0.001,
-      });
-      useEffect(() => {
-        Geolocation.getCurrentPosition((pos) => {
-          const crd = pos.coords;
-          setPosition({
-            latitude: crd.latitude,
-            longitude: crd.longitude,
-            latitudeDelta: 0.0421,
-            longitudeDelta: 0.0421,
-          });
-        }).catch((err) => {
-          console.log(err);
-        });
-      }, []);
-      const setData=()=>{
-        navigation.navigate('Home');
-      }
-  return (
-    <View style={styles.container}>
-    {/*Render our MapView*/}
-      <MapView
-        style={styles.map}
-        //specify our coordinates.
-        initialRegion={position}
-        showsUserLocation={true}
-      > 
-      <Marker coordinate={position} />
-          </MapView>
-          <CustomButton
-           title='Dashboard'
-           color='#0080ff'
-           onPressFunction={setData}/>
+    });
 
-      <Text style={styles.text}>Current latitude: {position.latitude}</Text>
-    <Text style={styles.text}>Current longitude: {position.longitude}</Text>
-    </View>
-  );
+    useEffect(() => {
+        createTable();
+
+        Geolocation.getCurrentPosition((pos) => {
+            const crd = pos.coords;
+            setPosition({
+                latitude: crd.latitude,
+                longitude: crd.longitude,
+                latitudeDelta: 0.0421,
+                longitudeDelta: 0.0421,
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+        getLocation();
+    }, []);
+
+    const createTable = () => {
+        db.transaction((tx) => {
+            tx.executeSql(
+                "CREATE TABLE IF NOT EXISTS "
+                + "Location "
+                + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, latitude REAL, longitude REAL);"
+            )
+        })
+    }
+
+    const setData = () => {
+        // navigation.navigate('Home');
+        //getLocation();
+        setInterval(() => getLocation(), 15000)
+        navigation.navigate('Home');
+    }
+    const getLocation = async () => {
+
+        try {
+
+            Geolocation.getCurrentPosition((pos) => {
+                const crd = pos.coords;
+                setPosition({
+                    latitude: crd.latitude,
+                    longitude: crd.longitude,
+                    latitudeDelta: 0.0421,
+                    longitudeDelta: 0.0421,
+                });
+            })
+            await db.transaction(async (tx) => {
+
+                await tx.executeSql(
+                    "INSERT INTO Location (latitude, longitude) VALUES (?,?)",
+                    [position.latitude, position.longitude]
+                );
+            })
+            //  navigation.navigate('Home');
+
+        } catch (error) {
+            console.log(error);
+        }
+
+
+    }
+    return (
+        <View style={styles.container}>
+            {/*Render our MapView*/}
+            <MapView
+                style={styles.map}
+                //specify our coordinates.
+                initialRegion={position}
+                showsUserLocation={true}
+            >
+                <Marker coordinate={position} />
+            </MapView>
+            <CustomButton
+                title='Dashboard'
+                color='#0080ff'
+                onPressFunction={setData} />
+
+            <Text style={styles.text}>Current latitude: {position.latitude}</Text>
+            <Text style={styles.text}>Current longitude: {position.longitude}</Text>
+        </View>
+    );
 }
 //create our styling code:
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    flex: 1, //the container will fill the whole screen.
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        flex: 1, //the container will fill the whole screen.
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
 });
